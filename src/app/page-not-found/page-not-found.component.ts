cceef62e5cd8f6e4cbc10268ef9c8c065725b4bf
Pedro0604 <pedrospadari0604@gmail.com>
2024-12-05 12:07:20 -0300

    Rutas base

---
+++ src/app/page-not-found/page-not-found.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-page-not-found',
+  imports: [],
+  templateUrl: './page-not-found.component.html',
+  standalone: true,
+})
+export class PageNotFoundComponent {
+
+}
