8cbe88b737106516f5bceeb49d7553d66b742d18
Pedro0604 <pedrospadari0604@gmail.com>
2024-12-09 17:44:26 -0300

    Añade componente menu form

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import {Routes} from '@angular/router';
 import {LoginComponent} from './login/login.component';
 import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
 import {MenuListComponent} from './menu-list/menu-list.component';
+import {MenuFormComponent} from './menu-form/menu-form.component';
 
 export const appRoutes: Routes = [
   {
@@ -12,6 +13,16 @@ export const appRoutes: Routes = [
     path: 'menu',
     component: MenuListComponent,
     title: 'Todos los menús'
+  },
+  {
+    path: 'menu/create',
+    component: MenuFormComponent,
+    title: 'Crear un menú'
+  },
+  {
+    path: 'menu/edit:id',
+    component: MenuFormComponent,
+    title: 'Editar un menú'
   },
   {
     path: '**', component: PageNotFoundComponent
---
+++ src/app/menu-form/menu-form.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-form',
+  imports: [],
+  templateUrl: './menu-form.component.html',
+  standalone: true,
+  styleUrl: './menu-form.component.css'
+})
+export class MenuFormComponent {
+  constructor() {}
+
+}
