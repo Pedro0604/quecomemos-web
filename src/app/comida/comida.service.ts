da9cb445b825e4206e14b1bc876c02c70f0ebc5e
Pedro0604 <pedrospadari0604@gmail.com>
2024-12-11 17:16:17 -0300

    Añade metodos a comidaService

--- src/app/comida/comida.service.ts
+++ src/app/comida/comida.service.ts
@@ -16,4 +16,16 @@ export class ComidaService {
   getComidas(): Observable<Comida[]> {
     return this.http.get<Comida[]>(this.apiUrl);
   }
+
+  getComidaById(id: string): Observable<Comida> {
+    return this.http.get<Comida>(this.apiUrl + "/" + id);
+  }
+
+  createComida(comida: Comida): Observable<Comida> {
+    return this.http.post<Comida>(this.apiUrl, comida);
+  }
+
+  updateComida(id: number, comida: Comida): Observable<Comida> {
+    return this.http.put<Comida>(this.apiUrl + "/" + id, comida);
+  }
 }
