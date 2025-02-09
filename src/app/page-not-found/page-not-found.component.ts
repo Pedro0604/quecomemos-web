278bd906d05233a785a6f0ec4762e483537af3aa
Pedro0604 <pedrospadari0604@gmail.com>
2025-02-09 11:18:44 -0300

    Mejora la página 404

--- src/app/page-not-found/page-not-found.component.ts
+++ src/app/page-not-found/page-not-found.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import {MatAnchor} from '@angular/material/button';
 
 @Component({
   selector: 'app-page-not-found',
-  imports: [],
+  imports: [
+    MatAnchor
+  ],
   templateUrl: './page-not-found.component.html',
   standalone: true,
 })
