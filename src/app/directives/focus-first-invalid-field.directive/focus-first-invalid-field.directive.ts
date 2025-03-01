5ddf5472402a7b2c89898da105234de2724cd4cb
Pedro0604 <pedrospadari0604@gmail.com>
2025-03-01 17:59:39 -0300

    Directiva para focus al primer elemento invalido

---
+++ src/app/directives/focus-first-invalid-field.directive/focus-first-invalid-field.directive.ts
@@ -0,0 +1,19 @@
+import {Directive, ElementRef, HostListener} from '@angular/core';
+
+@Directive({
+  standalone: true,
+  selector: 'form'
+})
+export class FocusFirstInvalidFieldDirective {
+
+  constructor(private el: ElementRef) {
+  }
+
+  @HostListener('submit')
+  public onSubmit(): void {
+    const invalidElements = this.el.nativeElement.querySelectorAll('.ng-invalid');
+    if (invalidElements.length > 0) {
+      invalidElements[0].querySelector('input, select, mat-select')?.focus();
+    }
+  }
+}
