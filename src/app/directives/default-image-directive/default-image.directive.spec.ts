3f750bfac75a66fc4fd910f563722bb1865cd7ea
Pedro0604 <pedrospadari0604@gmail.com>
2025-03-01 17:59:28 -0300

    Directiva para imagen default

---
+++ src/app/directives/default-image-directive/default-image.directive.spec.ts
@@ -0,0 +1,9 @@
+import { DefaultImageDirective } from './default-image.directive';
+
+describe('DefaultImageDirective', () => {
+  it('should create an instance', () => {
+    // @ts-ignore
+    const directive = new DefaultImageDirective();
+    expect(directive).toBeTruthy();
+  });
+});
