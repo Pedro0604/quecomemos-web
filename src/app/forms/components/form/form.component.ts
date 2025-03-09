9e4a466e6664e521f5ece86f8d04e3b1ec94f9d0
Pedro0604 <pedrospadari0604@gmail.com>
2025-03-09 18:52:47 -0300

    Corrige error con app-form

--- src/app/forms/components/form/form.component.ts
+++ src/app/forms/components/form/form.component.ts
@@ -17,7 +17,7 @@ import {
   standalone: true,
 })
 export class FormComponent {
-  @Input({required: true}) form!: FormGroup;
+  @Input({required: true}) formGroup!: FormGroup;
   @Input() maxWidth: string = 'max-w-2xl';
 
   @Output() submit = new EventEmitter<any>();
